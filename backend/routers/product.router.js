31cfa16841a974346c17924684ca50e4b8a7e1d6
ibrahim keskin <brhmkskn@outlook.com>
2025-02-24 15:45:39 +0100

    added product list pagination and get product by id

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -47,3 +47,54 @@ router.post("removeById", async(req,res)=>{
     });
 
 })
+
+// Pagination Product List
+router.post("/",async(req,res)=>{
+
+    response(res,async()=>{
+        const {pageNumber, pageSize, search}=req.body;
+
+        let productCount= await Product.find({
+            $or:[
+                {
+                    name: { $regex:search, $options:'i' }
+
+                }
+            ]
+        }).count();
+
+    let products = await Product.find({
+            $or:[
+                {
+                name: { $regex:search, $options:'i' }
+                }
+            ]
+        }).sort({name:1})
+        .populate("categories")
+        .skip((pageNumber-1)*pageSize)
+        .limit(pageSize);
+
+    let totalPageCount=Math.ceil(productCount/pageSize);
+    let model={
+        datas:products,
+        pageNumber:pageNumber,
+        pageSize:pageSize,
+        totalPageCount:totalPageCount,
+        isFirstPage:pageNumber==1?true:false,
+        isLastPage:totalPageCount==pageNumber?true:false
+    };
+
+        res.json(model);
+    });
+});
+
+// Get Product by id
+router.post("/getById",async(res,req)=>{
+    response(res,async()=>{
+        const{_id}=req.body;
+        let product = await Product.findById(_id);
+        res.json(product);
+    });
+});
+
+
